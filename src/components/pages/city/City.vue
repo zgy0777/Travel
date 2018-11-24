a520cec2c03ef4f53b185464853832dafe4f1066
zgy0777 <soldier76@qq.com>
2018-11-24 22:34:47 +0800

    add search to city

--- src/components/pages/city/City.vue
+++ src/components/pages/city/City.vue
@@ -1,19 +1,24 @@
 <template>
     <div>
         <city-header></city-header>
+        <city-search></city-search>
     </div>
 </template>
 
 <script>
 import CityHeader from "./components/Header.vue"
+import CitySearch from "./components/Search.vue"
+
 export default {
     name: "City",
     components:{
         CityHeader,
+        CitySearch,
     }
 }
 </script>
 
 <style lang="stylus"  scoped>
+
 
 </style>

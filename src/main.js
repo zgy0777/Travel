61ce6185b2a06bb84ae55fbb7778aa2f21716b59
zgy0777 <soldier76@qq.com>
2018-11-24 23:28:02 +0800

    add city-css

--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import App from './App'
 import router from './router' 
 import fastClick from 'fastClick'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
+
 import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
 import 'swiper/dist/css/swiper.css'
+
 
 
 Vue.config.productionTip = false

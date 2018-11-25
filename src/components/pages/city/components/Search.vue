6daa90cd72e0391e4bd91b1d4116b485b7fcc03b
zgy0777 <soldier76@qq.com>
2018-11-25 19:58:40 +0800

    finish search again

--- src/components/pages/city/components/Search.vue
+++ src/components/pages/city/components/Search.vue
@@ -6,7 +6,7 @@
         <div class="search-content" ref="search" v-show="keyword" >
             <ul>
                 <li class="search-item border-bottom"
-                 v-for="item of list" :key="item.id">{{item.name}}</li>
+                 v-for="item of list" :key="item.id" >{{item.name}}</li>
                  <li class="search-item border-bottom" v-show="hasNoData" >
                      没有找到匹配数据
                  </li>

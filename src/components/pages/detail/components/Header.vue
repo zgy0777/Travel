defdb50d058aced3b6e84cfc40adba9d5fb0112b
zgy0777 <soldier76@qq.com>
2018-11-27 20:44:59 +0800

    fix problem

--- src/components/pages/detail/components/Header.vue
+++ src/components/pages/detail/components/Header.vue
@@ -26,8 +26,12 @@
         activated(){
             window.addEventListener('scroll',this.handleScroll);
         },
+        deactivated(){
+            window.removeEventListener('scroll',this.handleScroll);
+        },
         methods:{
             handleScroll(){
+                console.log('scroll')
                 const top = document.documentElement.scrollTop;
                 if( top>60  ){
                     let opacity = top / 140;
